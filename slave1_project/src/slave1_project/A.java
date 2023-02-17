e1b621d637b7ec23df2d7fbf12d55d8d9bb523cb
Akshay <Akshay@host.docker.internal>
2023-02-17 13:01:13 +0530

    slave1 project

---
+++ slave1_project/src/slave1_project/A.java
@@ -0,0 +1,11 @@
+package slave1_project;
+
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println("Slave project");
+		System.out.println("helllo slave project");
+
+	}
+
+}
